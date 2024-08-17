01774170d9ceb05a88e20704fef8fd927593f39e
madalin11 <madalin.c.puiu@gmail.com>
2024-08-17 21:42:54 +0300

    update

--- FE/src/app/home-page/home-page.component.ts
+++ FE/src/app/home-page/home-page.component.ts
@@ -1,13 +1,18 @@
 import {Component, HostListener} from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
+import {NgClass, NgForOf, NgIf, NgOptimizedImage} from "@angular/common";
 import {MatProgressBar} from "@angular/material/progress-bar";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    MatProgressBar
+    MatProgressBar,
+    MatIcon,
+    NgForOf,
+    NgClass,
+    NgIf
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
