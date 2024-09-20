a20d331c2505796cbe6d0efa11ea8de43f2eb4f6
luyzaNA <146641612+luyzaNA@users.noreply.github.com>
2024-09-20 20:43:44 +0300

    update

--- FE/src/app/services/services.component.ts
+++ FE/src/app/services/services.component.ts
@@ -16,52 +16,53 @@ import {RouterLink} from "@angular/router";
 export class ServicesComponent {
   services = [
     {
-      name: 'Service 1',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
-      price: 100,
+      name: 'DECOPERTAT GRESIE/FAIANTA',
+      description: 'Serviciu de decopertare gresie, 30 lei/metru pătrat, pregătire eficientă a suprafeței pentru noi renovări.',
+      price: 30,
       image: 'assets/service-placeholder.png'
     },
     // Add more services here
     {
-      name: 'Service 2',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
-      price: 200,
+      name: 'PLACARE GRESIE/FAIANTA',
+      description: 'Serviciu de placare gresie și faianță, 90 lei/metru pătrat, oferind un design elegant și durabil pentru orice spațiu.',
+      price: 90,
       image: 'assets/service-placeholder.png'
     },
     {
-      name: 'Service 3',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
-      price: 300,
+      name: 'MANOPERA CARAMIDA APARENTA',
+      description: '\n' +
+        'Serviciu de montare cărămidă aparentă, 120 lei/metru pătrat, aducând un aspect rustic și autentic în interior sau exterior.',
+      price: 120,
       image: 'assets/service-placeholder.png'
     },
     {
-      name: 'Service 4',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
-      price: 400,
+      name: 'MANOPERA PLACARE MARMURA',
+      description: 'Serviciu de placare cu marmură, 130 lei/metru pătrat, oferind un aspect luxurious și sofisticat, ideal pentru orice spațiu elegant.',
+      price: 130,
       image: 'assets/service-placeholder.png'
     },
     {
-      name: 'Service 5',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
-      price: 500,
+      name: 'MANOPERA PLACARE GRANIT',
+      description: 'Serviciu de placare cu granit, 130 lei/metru pătrat, oferind robustețe și estetică superioară, ideal pentru proiecte deosebite și durabile.',
+      price: 130,
       image: 'assets/service-placeholder.png'
     },
     {
-      name: 'Service 6',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
-      price: 600,
+      name: 'MANOPERA PLACARE ONIX',
+      description: 'Serviciu de placare cu onix, 30 lei/metru pătrat, aducând un strop de lux și transparență în orice ambient, perfect pentru efecte decorative unice.',
+      price: 130,
       image: 'assets/service-placeholder.png'
     },
     {
-      name: 'Service 7',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
-      price: 700,
+      name: 'MANOPERA PLACARE TRAVERTIN',
+      description: 'Serviciu de placare cu travertin, 30 lei/metru pătrat, aducând un aspect natural și cald, ',
+      price: 130,
       image: 'assets/service-placeholder.png'
     },
     {
-      name: 'Service 8',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
-      price: 800,
+      name: 'PLACARI TREPTE GRESIE',
+      description: 'Placarea treptelor cu gresie, 100 lei/metru liniar, presupune pregătirea suprafeței, tăierea și montarea plăcilor de gresie, asigurând un finisaj uniform și estetic, cu atenție la detalii pentru siguranță și durabilitate.',
+      price: 100,
       image: 'assets/service-placeholder.png'
     },
     {
