ed41122fe74ac4af2a398011524a92325b2d2629
madalin11 <madalin.c.puiu@gmail.com>
2024-09-20 13:45:32 +0300

    fixed header and added footer page links

--- FE/src/app/footer/footer.component.ts
+++ FE/src/app/footer/footer.component.ts
@@ -1,17 +1,39 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 import {MatIcon} from "@angular/material/icon";
+import {Router, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    MatIcon
+    MatIcon,
+    RouterLink
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  constructor(private router: Router) {
+  }
 
+  scrollToSection(sectionId: string) {
+    if(this.router.url !== '/'){
+      this.router.navigate(['/']);
+    }
+    setTimeout(() => {
+      const element = document.getElementById(sectionId);
+      if (element) {
+        const headerOffset = sectionId != "home"? 70 : 500; // Change this value to your desired offset
+        const elementPosition = element.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+        window.scrollTo({
+          top: offsetPosition,
+          behavior: 'smooth'
+        });
+      }
+    }, 500)
+  }
 }
--- FE/src/app/home-page/home-page.component.ts
+++ FE/src/app/home-page/home-page.component.ts
@@ -2,7 +2,7 @@ import {Component, HostListener} from '@angular/core';
 import {NgClass, NgForOf, NgIf, NgOptimizedImage} from "@angular/common";
 import {MatProgressBar} from "@angular/material/progress-bar";
 import {MatIcon} from "@angular/material/icon";
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-home-page',
@@ -20,4 +20,44 @@ import {RouterLink} from "@angular/router";
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+  constructor(private router: Router){
+
+  }
+  services = [
+    {
+      name: 'Service 1',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
+      price: 100
+    },
+    // Add more services here
+    {
+      name: 'Service 2',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
+      price: 200
+    },
+    {
+      name: 'Service 3',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
+      price: 300
+    },
+  ];
+
+  scrollToSection(sectionId: string) {
+    if(this.router.url !== '/'){
+      this.router.navigate(['/']);
+    }
+    setTimeout(() => {
+      const element = document.getElementById(sectionId);
+      if (element) {
+        const headerOffset = sectionId != "home"? 70 : 500; // Change this value to your desired offset
+        const elementPosition = element.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+        window.scrollTo({
+          top: offsetPosition,
+          behavior: 'smooth'
+        });
+      }
+    }, 500)
+  }
 }
